c51fe0dabc9519865f2487e44cf1765b404f6c81
agalanov <agalanoff@gmail.com>
2025-11-30 15:07:01 +0300

    test: настройка Jest

---
+++ backend/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    roots: ['<rootDir>/src'],
+    setupFiles: ['<rootDir>/jest.setup.integration.ts'],
+};
+
+export default config;
